066306387c1d4c61d8510a385c51109198152616
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2024-04-28 16:37:03 +0800

    ci: isloate ci pipeline for videoProcessSVC for microservice structure (#7)
    * ci: isloate ci pipeline for videoProcessSVC for microservice structure
    Probllem: the old build.yaml stems from fullStackDev project, whose structure is quite different from this project. fullStackDev project currently is not "microservice" enough, where as videoProcessSVC is a standalone service.
    Solution: This new pipeline will lint the service code, build the docker image for production environment and then push to my docker hub repo.

--- videoProcessSVC/eslint.config.mjs
+++ videoProcessSVC/eslint.config.mjs
@@ -1,17 +1,20 @@
-import globals from "globals";
+import globals from 'globals'
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { FlatCompat } from '@eslint/eslintrc'
+import pluginJs from '@eslint/js'
 
 // mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({ baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended })
 
-export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
-  ...compat.extends("standard-with-typescript"),
-];
+export default {
+  ignores: ["eslint.config.mjs"], // Ignore the ESLint config file itself
+  overrides: [
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" }},
+    { languageOptions: { globals: globals.browser }},
+    ...compat.extends("standard-with-typescript"),
+  ]
+};
