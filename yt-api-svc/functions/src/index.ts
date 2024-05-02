46cc1044cf1ee80541312fdb6fa433ce0f2a7790
Hsien Heng Tommy Huang <44539518+thuang86714@users.noreply.github.com>
2024-05-02 10:54:18 +0800

    feat: add functions to generate SignedUrl and create user (#9)
    Since our bucket for raw video is private, we could not upload anything
    without a signed url. Incorporate GCP Bucket and Firebase to generate
    signed Url.
    As for users, add create function to create and store user info in
    Firestore
    
    TODO:
    refactor to eliminate repeated code

---
+++ yt-api-svc/functions/src/index.ts
@@ -0,0 +1,75 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+
+import {Storage} from "@google-cloud/storage";
+import { onCall } from "firebase-functions/v2/https";
+
+initializeApp();
+
+const firestore = new Firestore();
+const storage = new Storage();
+const rawVideoBucketName = "youtubeclone_raw";
+const videoCollectionId = "videos";
+
+export interface Video{
+    id?: string,
+    uid?: string,
+    filename?: string,
+    status?: "processing" | "processed",
+    title?: string,
+    description?: string
+}
+
+export const createUser = functions.auth.user().onCreate((user) => {
+    const userInfo = {
+        uid: user.uid,
+        email: user.email,
+        photoUrl: user.photoURL,
+    };
+
+    firestore.collection("user").doc(user.uid).set(userInfo);
+    logger.info(`User created: ${JSON.stringify(userInfo)}`);
+    return;
+});
+
+export const generateUploadUrl = onCall({maxInstances: 1}, async (request) => {
+    // Check if the user is authenticated
+    if(!request.auth){
+        throw new functions.https.HttpsError(
+            "failed-precondition",
+            "The functions must be called while authenticated."
+        );
+    }
+
+    const auth = request.auth;
+    const data = request.data;
+    const bucket = storage.bucket(rawVideoBucketName);
+    
+    // Generate a unique filename for upload
+    const fileName = `${auth.uid}-${Date.now()}.${data.fileExtension}`;
+    
+     // Get a v4 signed URL for uploading file
+     const [url] = await bucket.file(fileName).getSignedUrl({
+        version: "v4",
+        action: "write",
+        expires: Date.now() + 15 * 60 * 1000,
+     });
+    
+     return {url, fileName};
+});
+
+export const getVideo = onCall({maxInstances: 1}, async () =>{
+    const snapshot = 
+        await firestore.collection(videoCollectionId).limit(10).get();
+    return snapshot.docs.map((doc) => doc.data());
+});
